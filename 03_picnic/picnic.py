d904fa24299a8c2d67098ae2d81a1ec6c834b784
davyzhang3 <davyzhang325@gmail.com>
2021-06-22 21:46:47 -0400

    updated 03

---
+++ 03_picnic/picnic.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+Author : Dawei Zhang <davyzhang325@gmail.com>
+Date   : 2021-06-22
+Purpose: Rock the Casbah
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Rock the Casbah',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('item',
+                        metavar='str',
+                        nargs='+',
+                        help='Item(s) to bring')
+
+    parser.add_argument('-s',
+                        '--sorted',
+                        help='Sort the item (default:False)',
+                        action='store_true')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    items = args.item
+    num = len(items)
+
+    if args.sorted:
+        items.sort()
+
+    bringing = ''
+    if num == 1:
+        bringing = items[0]
+    elif num == 2:
+        bringing = ' and '.join(items)
+    else:
+        items[-1] = 'and ' + items[-1]
+        bringing = ', '.join(items)
+
+    print(f'You are bringing {bringing}.')
+
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
