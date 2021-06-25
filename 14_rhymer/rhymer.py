de5a5ba0d092235f6e939e6cb80b845dd0176799
davyzhang3 <davyzhang325@gmail.com>
2021-06-25 10:23:48 -0400

    updated 14

--- 14_rhymer/rhymer.py
+++ 14_rhymer/rhymer.py
@@ -6,7 +6,8 @@ Purpose: Rock the Casbah
 """
 
 import argparse
-
+import re
+import string
 
 # --------------------------------------------------
 def get_args():
@@ -28,10 +29,27 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
+    cluster = 'bl br ch cl cr dr fl fr gl gr pl pr sc sh sk sl sm sn sp st sw th tr tw thw wh wr sch scr shr sph spl spr squ str thr'.split()
+    prefixes = list(c for c in string.ascii_lowercase if c not in 'aeiou') + cluster
 
-def stemmer():
+    start, rest = stemmer(args.word)
+    if rest:
+        print('\n'.join(sorted([p + rest for p in prefixes if p != start])))
+    else:
+        print(f'Cannot rhyme "{args.word}"')
+def stemmer(word):
     """Return leading consonants (if any), and 'stem' of  word"""
-    pass
+   
+    consonants = ''.join([c for c in string.ascii_lowercase if c not in 'aeiou'])
+    word = word.lower()
+    match = re.match(f'([{consonants}]+)?([aeiou])(.*)', word)
+    if match:
+        p1 = match.group(1) or ''
+        p2 = match.group(2) or ''
+        p3 = match.group(3) or ''
+        return (p1, p2 + p3)
+    else:
+        return (word, '')
 
 def test_stemmer():
     """test stemmer """
